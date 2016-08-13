ba230ec85e9f4320f40d50c21e034f9ca886c70b
loxxo <water2ice@gmail.com>
2016-08-13 19:23:28 +0800

    init

--- crm/app/models.py
+++ crm/app/models.py
@@ -1,7 +1,46 @@
-"""
-Definition of models.
-"""
-
 from django.db import models
+from django.contrib.auth.models import User
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Developer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roles = models.ManyToManyField(Role)
+
+class Case(models.Model):
+    CASE_STATUS = (
+        (1, 'Pre'),
+        (2, 'Ing'),
+        (3, 'Past'),
+    )
+    name = models.CharField(max_length=50)
+    client = models.ForeignKey(Client)
+    status = models.IntegerField(choices=CASE_STATUS)
+    start_date = models.DateField()
+    expect_done_date = models.DateField()
+    done_date = models.DateField()
+    developers = models.ManyToManyField(Developer)
+
+class Comment(models.Model):
+    content =  models.CharField(max_length=50)
+    developer = models.ForeignKey(Developer)
+    case = models.ForeignKey(Case)
+    client = models.ForeignKey(Client)
+    date = models.DateField()
+
+class CaseDeveloper(models.Model):
+    case = models.ForeignKey(Case)
+    developer = models.ForeignKey(Developer)
+    role = models.CharField(max_length=50)
+    #models.OneToOneField(Case
+                         
+
 
 # Create your models here.
--- crm/app/views.py
+++ crm/app/views.py
@@ -44,3 +44,88 @@ def about(request):
             'year':datetime.now().year,
         }
     )
+
+def cases(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/cases.html',
+        {
+            'title':'项目',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def clients(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/cases.html',
+        {
+            'title':'项目',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def developers(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/developers.html',
+        {
+            'title':'开发者',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def developer(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/developers.html',
+        {
+            'title':'开发者',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def case(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/developers.html',
+        {
+            'title':'开发者',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def manager(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/developers.html',
+        {
+            'title':'开发者',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def client(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/developers.html',
+        {
+            'title':'开发者',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
